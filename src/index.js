33b3dfab722068613ec2036f12cd8863b05aa354
Ashbourn D'Cunha <94105472+cristan02@users.noreply.github.com>
2023-05-10 19:14:40 +0530

    removed extra files

--- src/index.js
+++ src/index.js
@@ -1,35 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-import './index.css';
-import Birthday from './Birthday/Birthday';
-import ReviewPage from './Review/ReviewPage';
+import './index.css'
+import Birthday from './Birthday/Birthday'
+import ReviewPage from './Review/ReviewPage'
 import Employees from './Employee/Employees'
 import Notes from './to_do/Notes'
 import Restaurant from './Restaurant/Restaurant'
-import Request from './Request/Request';
-import Coda from './Coda/Coda';
+import Request from './Request/Request'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import NavMenu from './NavMenu'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     {/* <Employees /> */}
     {/* <Birthday /> */}
-    {/* <ReviewPage /> */} 
+    {/* <ReviewPage />  */}
     {/* <Notes/> */}
-    {/* { <Restaurant /> } */}
+    {<Restaurant />}
     {/* {<Request/>} */}
-    
-    {<BrowserRouter>
-      {<Coda/>}
-    </BrowserRouter>}
-    
-    
-  </React.StrictMode>
-);
+
+    {/* <NavMenu/> */}
+  </React.StrictMode>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
